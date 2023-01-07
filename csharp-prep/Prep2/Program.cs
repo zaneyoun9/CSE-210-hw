b05f3d4781efdfd1099d41d2a42e8703626ea420
zaneyoun9 <113536094+zaneyoun9@users.noreply.github.com>
2023-01-07 16:09:18 -0700

    week 1 prep assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your score? ");
+        string userInput = Console.ReadLine();
+        int score = int.Parse(userInput);
+        string grade = " ";
+
+        if (score >= 90)
+        {
+            grade = "A";
+        }
+        else if (score >= 80)
+        {
+            grade = "B";
+        }
+        else if (score >= 70)
+        {
+            grade = "C";
+        }
+        else if (score >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        if (score >= 70)
+        {
+            Console.WriteLine($"Congratulations you passed the class and your grade was a(n) {grade}.");
+        }
+        else
+        {
+            Console.WriteLine($"Unfortunately your grade was a(n) {grade}. You did not pass the class, Good luck on your next attempt.");
+        }
     }
 }
